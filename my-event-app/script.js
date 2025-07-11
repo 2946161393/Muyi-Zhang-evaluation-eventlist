10c94077149c8d4decef867ed83ddaa4c10b8197
Your Name <you@example.com>
2025-07-11 17:32:53 -0400

    bew

--- my-event-app/script.js
+++ my-event-app/script.js
@@ -42,22 +42,22 @@ function draw() {
         en +
         "'></td><td><div class='action-buttons'>" +
         "<button class='action-btn save' onclick='save(" + e.id + ")'>" +
-        "<svg focusable='false' aria-hidden='true' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>" +
+        "<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>" +
         "<path d='M21,20V8.414a1,1,0,0,0-.293-.707L16.293,3.293A1,1,0,0,0,15.586,3H4A1,1,0,0,0,3,4V20a1,1,0,0,0,1,1H20A1,1,0,0,0,21,20ZM9,8h4a1,1,0,0,1,0,2H9A1,1,0,0,1,9,8Zm7,11H8V15a1,1,0,0,1,1-1h6a1,1,0,0,1,1,1Z'/>" +
         "</svg></button>" +
         "<button class='action-btn cancel' onclick='cancel(" + e.id + ")'>" +
-        "<svg focusable='false' aria-hidden='true' viewBox='0 0 32 32' version='1.1' xmlns='http://www.w3.org/2000/svg'>" +
+        "<svg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'>" +
         "<path d='M19.587 16.001l6.096 6.096c0.396 0.396 0.396 1.039 0 1.435l-2.151 2.151c-0.396 0.396-1.038 0.396-1.435 0l-6.097-6.096-6.097 6.096c-0.396 0.396-1.038 0.396-1.434 0l-2.152-2.151c-0.396-0.396-0.396-1.038 0-1.435l6.097-6.096-6.097-6.097c-0.396-0.396-0.396-1.039 0-1.435l2.153-2.151c0.396-0.396 1.038-0.396 1.434 0l6.096 6.097 6.097-6.097c0.396-0.396 1.038-0.396 1.435 0l2.151 2.152c0.396 0.396 0.396 1.038 0 1.435l-6.096 6.096z'/>" +
         "</svg></button></div></td>";
     } else {
       tr.innerHTML =
         "<td>" + n + "</td><td>" + s + "</td><td>" + en + "</td><td><div class='action-buttons'>" +
         "<button class='action-btn edit' onclick='edit(" + e.id + ")'>" +
-        "<svg focusable='false' aria-hidden='true' viewBox='0 0 24 24'>" +
+        "<svg viewBox='0 0 24 24'>" +
         "<path d='M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z'/>" +
         "</svg></button>" +
         "<button class='action-btn delete' onclick='del(" + e.id + ")'>" +
-        "<svg focusable='false' aria-hidden='true' viewBox='0 0 24 24' data-testid='DeleteIcon' aria-label='fontSize small'>" +
+        "<svg viewBox='0 0 24 24'>" +
         "<path d='M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z'/>" +
         "</svg></button></div></td>";
     }
@@ -79,11 +79,11 @@ function addForm() {
     "<td><input type='date' id='newE'></td>" +
     "<td><div class='action-buttons'>" +
     "<button class='action-btn save' onclick='add()'>" +
-    "<svg focusable='false' aria-hidden='true' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>" +
+    "<svg viewBox='0 0 24 24'>" +
     "<path d='M21,20V8.414a1,1,0,0,0-.293-.707L16.293,3.293A1,1,0,0,0,15.586,3H4A1,1,0,0,0,3,4V20a1,1,0,0,0,1,1H20A1,1,0,0,0,21,20ZM9,8h4a1,1,0,0,1,0,2H9A1,1,0,0,1,9,8Zm7,11H8V15a1,1,0,0,1,1-1h6a1,1,0,0,1,1,1Z'/>" +
     "</svg></button>" +
     "<button class='action-btn cancel' onclick='cancelAdd()'>" +
-    "<svg focusable='false' aria-hidden='true' viewBox='0 0 32 32' version='1.1' xmlns='http://www.w3.org/2000/svg'>" +
+    "<svg viewBox='0 0 32 32'>" +
     "<path d='M19.587 16.001l6.096 6.096c0.396 0.396 0.396 1.039 0 1.435l-2.151 2.151c-0.396 0.396-1.038 0.396-1.435 0l-6.097-6.096-6.097 6.096c-0.396 0.396-1.038 0.396-1.434 0l-2.152-2.151c-0.396-0.396-0.396-1.038 0-1.435l6.097-6.096-6.097-6.097c-0.396-0.396-0.396-1.039 0-1.435l2.153-2.151c0.396-0.396 1.038-0.396 1.434 0l6.096 6.097 6.097-6.097c0.396-0.396 1.038-0.396 1.435 0l2.151 2.152c0.396 0.396 0.396 1.038 0 1.435l-6.096 6.096z'/>" +
     "</svg></button></div></td>";
   tb.appendChild(tr);
